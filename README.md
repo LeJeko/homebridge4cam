2a6bcdb835531f6f8abc05facc5d2cf528d01698
LeJeko <jeko@mac.com>
2019-04-11 00:54:21 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,220 @@
+# Homebridge4Cam
+
+### Easy install of homebridge on a headless* Raspberry Pi to bring IP Camera stream in HomeKit
+
+**\* Headless mean that you don't need to connect the raspi to a screen and a keyboard (nor an Ethernet cable) to configure it after restoring.**   
+Following steps explain how to activate SSH and configure Wi-Fi to manage it remotely.
+
+## Why homebridge4cam ?
+There isn't a lot of HomeKit compatible IP Camera. But there are a lot of cheaper ones, even in China ;)  
+Homebridge4cam will install all needed dependencies, automatically set up the homebridge server and expose it in a Web UI on port 8080.  
+All you need to do before is to edit *config.json* with the Web UI and add your [specific camera configuration](https://github.com/KhaosT/homebridge-camera-ffmpeg/wiki/Tested-Configurations) in the "camera-ffmpeg" platform section.  
+
+If you have a Pi camera connected (and activated, see below), it will be automatically added to homebridge and accessible in Home App.  
+Otherwise, a *"Fake Camera"* is created so that you can check if it is visible in the Home App.
+
+## Can I install homebridge4cam on an already setup Raspi ? 
+
+Yes
+
+* If you've never set up homebridge, you can jump to step 8.
+* If you have already a homebridge installation, it will update it but
+ leave config.json untouched.
+
+## What will be installed ? 
+
+* ffmpeg
+* node.js
+* homebridge
+* homebridge-camera-ffmpeg
+* homebridge-config-ui-x
+* homebridge-raspberrypi-temperature
+* homebridge-people
+
+## Manage homebridge
+
+Web UI: [http://raspberrypi.local:8080](http://raspberrypi.local:8080)
+
+iOS App: [Homebridge for RaspberryPi](https://itunes.apple.com/us/app/homebridge-for-raspberrypi/id1123183713?mt=8).
+
+* Crete a new connection and connect with user *pi*
+* Choose ***Existing installation***
+* Set config.json path ```/home/pi/.homebridge/config.json``` 
+* Use systemd
+* Use journalctl
+
+#=============================
+## Start install Raspi headless from scratch
+
+You need:
+
+* Raspi (tested with Raspberry Pi 3 Model B Plus Rev 1.3)
+* WiFi or Ethernet cable with Internet connection
+* Micro SD Card
+* Terminal (or [PuTTY](https://www.putty.org))
+
+[Source](https://hackernoon.com/raspberry-pi-headless-install-462ccabd75d0)
+
+### 1. Download Raspbian Image
+Head on over [here](https://www.raspberrypi.org/downloads/raspbian/) to grab a copy of the Raspbian image.
+
+The “Lite” version will do: [https://downloads.raspberrypi.org/raspbian_lite_latest](https://downloads.raspberrypi.org/raspbian_lite_latest)
+
+### 2. Write Image to SD Card
+Write the image to SD card. You can find detailed instructions [here](https://www.raspberrypi.org/documentation/installation/installing-images/README.md).
+
+Etcher is a graphical SD card writing tool that works on Mac OS, Linux and Windows, and is the easiest option for most users. Etcher also supports writing images directly from the zip file, without any unzipping required. To write your image with Etcher:
+
+* Download [Etcher](https://etcher.io/) and install it.
+* Connect an SD card reader with the SD card inside.
+* Open Etcher and select from your hard drive the Raspberry Pi .img or .zip file you wish to write to the SD card.
+* Select the SD card you wish to write your image to.
+* Review your selections and click 'Flash!' to begin writing data to the SD card.
+
+### 3. Enable SSH after install
+Enable SSH by placing a file named “ssh” (without any extension) in the boot partition of the SD card.
+On macOS terminal:
+
+```
+$ touch /Volumes/boot/ssh
+```
+
+### 4. Preconfiguring a WiFi network (else connect Ethernet cable)
+If you already know your WiFi details, add a file named wpa_supplicant.conf in the boot partition of the SD card. It look like this:
+
+```
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=CH
+
+network={ 
+    ssid="mySSID1" 
+    psk="myPwd2" 
+}
+network={ 
+    ssid="mySSID2" 
+    psk="myPwd2" 
+}
+```
+
+On macOS terminal:
+
+```
+$ nano /Volumes/boot/wpa_supplicant.conf
+```
+then adapt the code above and copy-paste.
+
+### 5. Enabling Pi camera
+
+To avoid one more reboot later, add *"start_x=1"* and *"gpu_mem=128"* to /boot/config.txt
+
+On macOS terminal:
+
+```
+$ echo -e "start_x=1\ngpu_mem=128" >> /Volumes/boot/config.txt
+```
+
+
+## **Insert SD Card to your Pi and restore it**
+
+*Install will take les than 1 min (Raspbian Stretch Lite on Raspberry Pi 3 Model B Plus Rev 1.3)*
+
+## Prepare you Raspi
+Open a Terminal on your computer.
+
+### 6. Find your Pi on the network
+
+Try to access it with its Bonjour name: ```
+$ ping raspberrypi.local
+```
+
+* if your have more than one, the name ident like this:
+```
+$ ping raspberrypi-2.local
+```
+
+else, if host is in your ARP table, you can try find its IP with this command: ```
+$ arp -an | grep -i b8:27:eb
+```
+
+### 7. Connect with SSH and configure
+```
+$ ssh pi@raspberrypi.local
+```
+(default password is *raspberry* )
+
+For security you should change the password::
+```
+$ passwd
+```
+
+### 8. Configure
+
+```
+$ sudo raspi-config
+```
+
+* go to **Localisation options**
+	* **Change Timezone** according to yours
+	* *[optional]* **Change Locale** (default is en_GB.UTF-8).  
+		
+### 9. Pi Camera (if not done in step 5)
+```
+$ sudo raspi-config
+```
+
+* If you haven't done in step 5, ensure the Camera interface is enabled in **Interfacing options -> Camera**
+
+	* **Activating Camera interface will require restarting. before continue**  
+	The script is only able to install the driver if the camera interface is enabled.
+
+If a Pi camera is connected, it will automatically be configured in homebridge.
+
+[Pi camera useful link](https://github.com/KhaosT/homebridge-camera-ffmpeg/issues/93#issuecomment-314479017)
+
+### 10. Download homebridge4cam script 
+
+```
+$ wget https://github.com/LeJeko/homebridge4cam/raw/master/homegridge4cam.sh
+```
+and set it executable.
+```
+$ chmod +x homebridge4cam.sh
+```
+
+**Do not force execution** with ```sudo sh script.sh``` because this will use *sh* shell instead of *bash* which is necessary for some features.
+
+### 11. Launch installation
+
+```
+$ sudo ./homebridge4cam.sh
+```
+
+You can follow the full scrolling log or a light version from another session with this command:
+
+```
+ssh -t pi@raspberrypi.local 'tail -f homebridge4cam.log'
+```
+
+### 12. Wait...
+
+Full script execution takes about 10 min with Ethernet cable (and about 30 min through Wi-Fi !?!).
+
+Script terminates with a *SUCCESS* or *ERROR* message.
+
+### 13. Add automatically created accessories to Home App
+
+* Go to the web UI and login (admin/admin): [http://raspberry.local:8080](http://raspberry.local:8080) 
+* You can now see the PIN code
+* Launch Home App in your iOS Device and add an accessory
+* Do not scan the QR code, choose **No code or can't scan code** option instead
+* You can see a bridge named **homebridge** and a camera named **Fake Camera** or **Pi Cam**
+* Add each and give PIN when asked
+* The bridge includes an accessory that gives the temperature of the Raspi.
+
+### 14. Add your own IP camera stream
+
+Edit config.json: [http://raspberry.local:8080/config](http://raspberry.local:8080/config)
+
+Tested IP Camera configurations:  
+[https://github.com/KhaosT/homebridge-camera-ffmpeg/wiki/Tested-Configurations](https://github.com/KhaosT/homebridge-camera-ffmpeg/wiki/Tested-Configurations).
---
+++ homebridge4cam.sh
@@ -0,0 +1,337 @@
+#!/bin/bash
+## © @LeJeko
+##
+## 2019.04.10 : v1.0 : First release
+##
+## Homebridge4cam : ready for IP Camera and Web UI
+## 
+
+# Initializing variables
+SECONDS=0
+LAST_TIME=0
+ELAPSED_TIME=0
+USERNAME=pi
+INSTALL_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+LOG_PATH="$INSTALL_PATH/homebridge4cam.log"
+SOURCES_PATH="https://github.com/LeJeko/homebridge4cam/raw/master/assets"
+
+cd $INSTALL_PATH
+
+# Check sudo
+if [ "$USER" != "root" ]; then
+        echo "Script must be run as root."
+        echo "`date`  ==  Script must be run as root." >> $LOG_PATH
+        echo "Use: $ sudo ./homebridge4cam.sh"
+        echo "`date`  ==  Use: $ sudo ./homebridge4cam.sh" >> $LOG_PATH
+        exit -1
+fi
+
+# Set function
+show_time () {
+    num=$1
+    min=0
+    hour=0
+    day=0
+    if((num>59));then
+        ((sec=num%60))
+        ((num=num/60))
+        if((num>59));then
+            ((min=num%60))
+            ((num=num/60))
+            if((num>23));then
+                ((hour=num%24))
+                ((day=num/24))
+            else
+                ((hour=num))
+            fi
+        else
+            ((min=num))
+        fi
+    else
+        ((sec=num))
+    fi
+    # formatting 00
+    if [ ${#day} = 1 ];then day="0$day";fi
+    if [ ${#hour} = 1 ];then hour="0$hour";fi
+    if [ ${#min} = 1 ];then min="0$min";fi
+    if [ ${#sec} = 1 ];then sec="0$sec";fi
+#    echo "$day"d "$hour"h "$min"m "$sec"s
+    echo "$hour"h "$min"m "$sec"s
+}
+
+print_time () {
+	TIMESTAMP=$SECONDS
+	ELAPSED_TIME=$(($TIMESTAMP - $LAST_TIME))
+	echo -e "`date`  ==  Duration: `show_time $ELAPSED_TIME` | Total: `show_time $SECONDS`\n"
+	echo -e "`date`      `show_time $ELAPSED_TIME` | Total: `show_time $SECONDS`\n" >> $LOG_PATH
+	LAST_TIME=$((TIMESTAMP))
+}
+
+# Starting install
+echo "***************************************************"
+echo "Homebridge4cam => INSTALLATION STARTING"
+echo "`date`"
+echo "***************************************************"
+echo "=================================" >> $LOG_PATH
+echo "==       homebridge4cam        ==" >> $LOG_PATH
+echo "=================================" >> $LOG_PATH
+echo "`date`  ==  ** Starting install**" >> $LOG_PATH
+
+# Ensure silent install
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND
+
+# Check user rights
+SUDOERS="/etc/sudoers"
+if grep -q "$USERNAME ALL=(ALL) NOPASSWD: ALL" "$SUDOERS"; then
+	echo "Sudoers entry exist"
+else
+	echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> $SUDOERS
+fi
+# Install gpio if none
+if [ "`which gpio`" = "" ]; then
+	echo "***************************************************"
+	echo "INSTALLING GPIO"
+	echo "***************************************************"
+	echo ""
+	echo "`date`  ==  Start installing gpio" >> $LOG_PATH
+	wget https://lion.drogon.net/wiringpi-2.50-1.deb
+	sudo dpkg -i wiringpi-2.50-1.deb
+	rm -f wiringpi-2.50-1.deb
+fi
+
+# Preparation in case of reinstallation (otherwhise cp node js will crash)
+killall homebridge
+
+echo "***************************************************"
+echo "PI INFORMATION"
+echo "***************************************************"
+echo "`date`  ==  `gpio -v | grep "*-->"`" >> $LOG_PATH
+gpio -v | grep "*-->"
+uname -a
+
+print_time
+# Pi Camera - Loading driver before upgrading system to avoid module installation error
+get_camera=`vcgencmd get_camera`
+camera_states=($get_camera)
+echo "`date`  ==  Pi cam $get_camera"
+echo "`date`  ==  Pi cam $get_camera" >> $LOG_PATH
+
+if [ "${camera_states[@]:0:1}" = "supported=1" ]; then
+	echo "***************************************************"
+	echo "INSTALLING PI CAMERA DRIVER"
+	echo "***************************************************"
+	echo "`date`  ==  Installing PiCam driver" >> $LOG_PATH
+	echo "" >> $LOG_PATH
+	modprobe bcm2835-v4l2
+	if ! grep -Fxq "bcm2835-v4l2" /etc/modules; then
+		echo "bcm2835-v4l2" >> /etc/modules
+	fi
+fi
+
+echo "***************************************************"
+echo "UPDATING & UPGRADING"
+echo "***************************************************"
+echo "`date`  ==  Start updating & upgrading" >> $LOG_PATH
+apt-get -yq update
+apt-get -yq upgrade --show-progress
+
+# Remove lock file to make sure installations succeed
+rm /var/cache/apt/archives/lock
+rm /var/lib/dpkg/lock
+dpkg --configure -a
+
+# Prevent Unable to locate package Errors --> homebridge: command not found
+apt-get -yq update
+
+print_time
+# Important installations
+echo "***************************************************"
+echo "INSTALLING OTHER SOFTWARE PACKAGES AND COMPILER"
+echo "***************************************************"
+echo "`date`  ==  Start installing other software packages and compiler" >> $LOG_PATH
+apt-get -yq install git make --show-progress
+apt-get -yq install sysstat --show-progress
+
+# Install Compiler
+apt-get -yq install gcc-4.9 g++-4.9 --show-progress
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+
+print_time
+# Install NODE JS
+echo "***************************************************"
+echo "INSTALLING NODE JS"
+echo "***************************************************"
+echo "`date`  ==  Start installing node js" >> $LOG_PATH
+arch=`uname -m`
+node_version=`node --version`
+if ! [ "$node_version" = "v9.9.0" ]; then
+	if [ "$arch" = "armv6l" ]; then
+    	echo "Hardware: RaspberryPi Zero or 1"
+    	wget https://nodejs.org/dist/v9.9.0/node-v9.9.0-linux-armv6l.tar.gz
+		tar -xf node-v9.9.0-linux-armv6l.tar.gz
+		rm node-v9.9.0-linux-armv6l.tar.gz
+		cd node-v9.9.0-linux-armv6l
+	else
+    	echo "Hardware: RaspberryPi 2 or higher"
+    	wget https://nodejs.org/dist/v9.9.0/node-v9.9.0-linux-armv7l.tar.gz
+		tar -xf node-v9.9.0-linux-armv7l.tar.gz
+		rm node-v9.9.0-linux-armv7l.tar.gz
+		cd node-v9.9.0-linux-armv7l
+	fi
+
+	echo "***************************************************"
+	echo "COPYING NODE FILES"
+	echo "***************************************************"
+	killall homebridge
+	cp -R * /usr/local/
+	cd ..
+	rm -rf node-v9.9.0-linux-armv*
+fi
+
+echo " INSTALLED NODE WITH NPM VERSION: "
+npm -v
+echo ""
+
+print_time
+# Install Libs
+echo "***************************************************"
+echo "INSTALLING LIBS"
+echo "***************************************************"
+echo "`date`  ==  Start installing libs" >> $LOG_PATH
+apt-get -yq install avahi-daemon --show-progress
+apt-get -yq install avahi-discover --show-progress
+apt-get -yq install libnss-mdns --show-progress
+apt-get -yq install libavahi-compat-libdnssd-dev --show-progress
+apt-get -yq install build-essential --show-progress
+apt-get -yq install git --show-progress
+
+print_time
+# Install FFmpeg
+
+echo "***************************************************"
+echo "INSTALLING FFMPEG"
+echo "***************************************************"
+echo "`date`  ==  Start installing FFMpeg" >> $LOG_PATH
+
+sudo apt-get -yq install ffmpeg --show-progress
+
+print_time
+# Install Homebridge
+echo "***************************************************"
+echo "INSTALLING HOMEBRIDGE "
+echo "***************************************************"
+echo "`date`  ==  Start installing homebridge" >> $LOG_PATH
+npm install -g --unsafe-perm homebridge
+
+print_time
+# Install Homebridge plugins
+echo "***************************************************"
+echo "INSTALLING HOMEBRIDGE PLUGINS"
+echo "***************************************************"
+echo "`date`  ==  Start installing homebridge plugins" >> $LOG_PATH
+npm install --unsafe-perm -g homebridge-config-ui-x
+npm install --unsafe-perm -g homebridge-raspberrypi-temperature
+npm install --unsafe-perm -g homebridge-camera-ffmpeg
+npm install --unsafe-perm -g homebridge-people
+
+# back to user home
+cd /home/$USERNAME
+
+print_time
+# homebridge config json
+if ! [ -f .homebridge/config.json ]
+then
+	echo "`date`  ==  Start configuring config.json" >> $LOG_PATH
+	echo "***************************************************"
+	echo "CREATING CONFIG.JSON"
+	echo "***************************************************"
+	
+	mkdir .homebridge
+	
+	if [ "${camera_states[@]:1:1}" = "detected=1" ]; then
+		wget $SOURCES_PATH/config_picam.json -O .homebridge/config.json
+	else
+		wget $SOURCES_PATH/config.json -O .homebridge/config.json
+		wget $SOURCES_PATH/FakeCamera.png -O .homebridge/FakeCamera.png
+		wget $SOURCES_PATH/FakeCamera.gif -O .homebridge/FakeCamera.gif
+	fi
+
+	RANDOMTWODIGITS=$(shuf -i 10-99 -n 1)
+	sed -i "s#!RANDOMTWODIGITS!#$RANDOMTWODIGITS#g" .homebridge/config.json
+	RANDOMTHREEDIGITS=$(shuf -i 100-999 -n 1)
+	sed -i "s#!RANDOMTHREEDIGITS!#$RANDOMTHREEDIGITS#g" .homebridge/config.json
+else
+	echo "`date`  ==  ** config.json exist, no modifications done!"
+	echo "`date`  ==  ** config.json exist, no modifications done!" >> $LOG_PATH
+fi
+
+# homebridge service
+echo "`date`  ==  Start configuring homebridge service" >> $LOG_PATH
+echo "***************************************************"
+echo "CREATING HOMEBRIDGE SERVICE (SYSTEMD)"
+echo "***************************************************"
+
+wget $SOURCES_PATH/homebridge -O /etc/default/homebridge
+wget $SOURCES_PATH/homebridge.service -O /etc/systemd/system/homebridge.service
+chmod 755 /etc/default/homebridge
+chmod 755 /etc/systemd/system/homebridge.service
+systemctl daemon-reload
+systemctl enable homebridge
+
+# owning some paths to ensure can be managed by other tools
+echo -e "\n`date`  ==  Owning paths\n" >> $LOG_PATH
+chown -R $USERNAME:$USERNAME /home/$USERNAME
+chown -R $USERNAME:$USERNAME /etc/wpa_supplicant/wpa_supplicant.conf
+
+echo "***************************************************"
+echo "STARTING HOMEBRIDGE"
+echo "***************************************************"
+echo "`date`  ==  Starting homebridge" >> $LOG_PATH
+systemctl start homebridge
+
+wait_time=5 # seconds
+temp_cnt=${wait_time}
+while [[ ${temp_cnt} -gt 0 ]];
+do
+	printf "\r==>> waiting homebridge to start (%2d sec)" ${temp_cnt}
+	sleep 1
+	((temp_cnt--))
+done; echo ""
+
+running=`systemctl status homebridge |grep -e "active (running)"`
+
+if [ "$running" = "" ]
+	then
+		echo "`date`  ==  *ERROR* : Homebridge can't start" >> $LOG_PATH
+		echo "==================================================="
+		echo "***************************************************"
+		echo ""
+		echo "==> *ERROR* !! : Homebridge can't start"
+		echo "==> See log below or $ journalctl -u homebridge"
+		echo ""
+		echo "***************************************************"
+		echo "==================================================="
+	else
+		raspi_ip=`hostname -I | sed 's/ //g'`
+		echo -e "\n`date`  ==  *SUCCESS* : Homebridge is up and running" >> $LOG_PATH
+		echo -e "`date`  ==  Web UI: http://$raspi_ip:8080 (admin/admin)\n" >> $LOG_PATH
+		echo "==================================================="
+		echo "***************************************************"
+		echo ""
+		echo "==>   *SUCCESS* !! : Homebridge is up and running"
+		echo "==>   Web UI: http://$raspi_ip:8080 (admin/admin)"
+		echo ""
+		echo "***************************************************"
+		echo "==================================================="
+fi
+
+# Finish
+echo "`date`  ==  Cleaning logs..." >> $LOG_PATH
+cat /dev/null > ~/.bash_history
+echo "FINISH!"
+echo "`date`  ==  ** Script finished!**" >> $LOG_PATH
+print_time
+
+exit 0
